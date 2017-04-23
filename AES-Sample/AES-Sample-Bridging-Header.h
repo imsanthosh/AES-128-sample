0d7cffb317ce5a03e8c3ac03e922537b99a40d16
Santhosh K <Santhosh.K@hotstar.com>
2017-04-23 17:24:01 +0530

    AES-iOS-sample, performs encryption and decryption on nsdata

---
+++ AES-Sample/AES-Sample-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  AES-Sample-Bridging-Header.h
+//  AES-Sample
+//
+//  Created by K, Santhosh on 23/04/17.
+//  Copyright © 2017 K, Santhosh. All rights reserved.
+//
+
+#ifndef AES_Sample_Bridging_Header_h
+#define AES_Sample_Bridging_Header_h
+
+
+#endif /* AES_Sample_Bridging_Header_h */
---
+++ AES-Sample/include/Cryptor/Cryptor.h
@@ -0,0 +1,22 @@
+//
+//  Cryptor.h
+//  Cryptor
+//
+//  Created by K, Santhosh on 23/04/17.
+//  Copyright © 2017 K, Santhosh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cryptor : NSObject
+
++ (instancetype _Nullable )new OBJC_UNAVAILABLE("Use shared instance.");
+
+- (instancetype _Nullable )init OBJC_UNAVAILABLE("Use shared instance.");
+
++ (instancetype _Nonnull)sharedInstance;
+
+- (NSData * _Nullable)encryptedDataOf:(NSData * _Nonnull)inputData withKey:(NSString * _Nullable)key;
+- (NSData * _Nullable)decryptedDataOf:(NSData * _Nonnull)inputData withKey:(NSString * _Nullable)key;
+
+@end
